6cc7e4f8721460e5ea389f4e73991b251721881d
paolo.spadoni <Paolo>
2018-11-06 16:39:25 +0000

    form template driven assignment

--- forms-td-assignment/src/app/app.component.ts
+++ forms-td-assignment/src/app/app.component.ts
@@ -1,15 +1,32 @@
-import { Component , ViewChild} from '@angular/core';
-import { NgForm} from '@angular/forms';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   @ViewChild('f') form: NgForm;
+  @ViewChild('selectbox') selectbox: ElementRef;
+  subscription: any;
+  subscriptions: any[] = [{
+    code: 'A',
+    description: 'Advanced'
+  }, {
+    code: 'B',
+    description: 'Basic'
+  }, {
+    code: 'P',
+    description: 'Pro'
+  }];
 
-  submit(){
-    console.log('formData = ' + this.form.value)
-  };
+  submit() {
+    console.log('password = ' + this.form.value.password +
+    ', subscription = ' + this.form.value.subscription + ', email = ' + this.form.value.email );
+  }
+
+  ngOnInit() {
+    this.subscription = 'A';
+  }
 }
