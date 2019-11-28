e7252afe6118541b2bea741b0fe86a455b73e156
root <paolo_spadoni@yahoo.it>
2019-11-28 06:24:53 +0100

    ngrx

---
+++ angular-ngrx/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { UsersComponent } from './containers/users/users.components';
+import { UserComponent } from './containers/user/user.component';
+
+const routes: Routes = [
+  { path: 'users', component: UsersComponent },
+  { path: 'user/:id', component: UserComponent },
+  { path: '', redirectTo: '/users', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ angular-ngrx/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
+import { BrowserModule } from '@angular/platform-browser';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+
+import { appReducers } from './store/reducers/app.reducers';
+import { environment } from '../environments/environment';
+import { AppRoutingModule } from './app-routing.module';
+import { ConfigEffects } from './store/effects/config.effects';
+import { UserEffects } from './store/effects/user.effects';
+import { AppComponent } from './app.component';
+import { UserService } from './services/user.service';
+import { UsersComponent as UsersContainerComponent } from './containers/users/users.components';
+import { UsersComponent } from './components/users/users.components';
+/*import { UserComponent } from './containers/user/user.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
+*/
+@NgModule({
+  declarations: [
+    AppComponent,
+    UsersContainerComponent,
+    UsersComponent,
+    /*UserComponent,
+    UserDetailsComponent*/
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    StoreModule.forRoot(appReducers),
+    EffectsModule.forRoot([UserEffects, ConfigEffects]),
+    StoreRouterConnectingModule.forRoot({ stateKey: 'router' }),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    AppRoutingModule
+  ],
+  providers: [UserService],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
